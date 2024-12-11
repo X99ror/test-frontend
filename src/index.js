0e07396c4dd80348df72566a972915d9ebf37b98
X99ror <144711429+X99ror@users.noreply.github.com>
2024-12-11 19:02:41 +0530

    Fix missing reportWebVitals or remove unnecessary import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/ReactToastify.css';
 
@@ -10,9 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
